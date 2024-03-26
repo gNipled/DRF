c3fea6ea1f7b5039de5c959bda4d4b1a9e435b42
Aleksei Ponomarenko <lehapon92@gmail.com>
2024-03-27 04:46:03 +0900

    added validator

---
+++ lms/validators.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+YOUTUBE_LINKS = ('youtube.com', 'youtu.be',)
+
+
+class LinkValidator:
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        tmp_val = dict(value).get(self.field)
+        for link in YOUTUBE_LINKS:
+            if link in tmp_val.lower:
+                break
+        raise serializers.ValidationError('Wrong link, please use youtube for videos')

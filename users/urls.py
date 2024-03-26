befed426462e2a05f371a8e19b8c256507c12a46
Aleksei Ponomarenko <lehapon92@gmail.com>
2024-03-27 05:21:20 +0900

    completed 25.2.2

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views import PaymentDateAPIListView
+from users.views import PaymentDateAPIListView, SubscriptionsAPIView
 
 urlpatterns = [
-    path('payments/', PaymentDateAPIListView.as_view())
+    path('payments/', PaymentDateAPIListView.as_view()),
+    path('subscriptions/', SubscriptionsAPIView.as_view(), name='subscribe'),
 ]
